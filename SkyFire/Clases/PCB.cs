b304dd1131d623af4e9e1c2923b7600593c42ae6
LobedGaming <86919211+LobedGaming@users.noreply.github.com>
2021-12-26 11:42:41 -0400

    Add files via upload

---
+++ SkyFire/Clases/PCB.cs
@@ -0,0 +1,23 @@
+﻿namespace SkyFire
+{
+    
+    class PCB
+    {
+        public int PID { get; set; }
+        public int DIR { get; set; }
+        public int TIPO { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int ANCHO { get; set; }
+        public int ALTO { get; set; }
+        public string COLOR { get; set; }
+        public int HORA { get; set; }
+        public int RETARDO { get; set; }
+        public bool PAREDD { get; set; }
+        public bool PAREDIZ { get; set; }
+        public bool PISO { get; set; }
+        public bool TECHO { get; set; }
+        public bool CHOCADo { get; set; }
+        public int vida { get; set; }
+    }
+}
